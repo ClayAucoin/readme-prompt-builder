d36cdf59cd70f26d0bb780771ddea8aa5ffd6bfb
ClayAucoin <clay0aucoin@gmail.com>
2026-01-05 14:18:21 -0600

    made change to express.static command

--- src/server.js
+++ src/server.js
@@ -2,13 +2,18 @@
 
 import express from "express";
 import path from "path";
+import { fileURLToPath } from "url";
 
 const app = express();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const PORT = Number(process.env.PORT || 3101);
 const HOST = process.env.HOST || "0.0.0.0";
 
-app.use(express.static(path.join(process.cwd(), "public")));
+app.use(express.static(path.join(__dirname, "..", "public")));
+// app.use(express.static(path.join(process.cwd(), "public")));
 
 app.get("/api/health", (req, res) => {
   res.json({ ok: true });
